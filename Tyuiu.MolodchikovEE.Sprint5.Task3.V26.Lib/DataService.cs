72d858070d6b7ee715584a8a2490cbccb992f20c
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-10-07 19:01:14 +0500

    Task3

--- Tyuiu.MolodchikovEE.Sprint5.Task3.V26.Lib/DataService.cs
+++ Tyuiu.MolodchikovEE.Sprint5.Task3.V26.Lib/DataService.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Text;
-using tyuiu.cources.programming.interfaces.Sprint5;
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
 namespace Tyuiu.MolodchikovEE.Sprint5.Task3.V26.Lib
 {
     public class DataService : ISprint5Task3V26
