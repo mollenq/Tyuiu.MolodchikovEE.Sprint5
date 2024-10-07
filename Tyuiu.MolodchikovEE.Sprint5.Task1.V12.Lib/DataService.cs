69401980a13b8b738753b0e509fbd4dfac444e4c
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-10-07 16:39:26 +0500

    Task1

--- Tyuiu.MolodchikovEE.Sprint5.Task1.V12.Lib/DataService.cs
+++ Tyuiu.MolodchikovEE.Sprint5.Task1.V12.Lib/DataService.cs
@@ -1,4 +1,6 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint5;
+﻿using System.Linq;
+using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint5;
 
 namespace Tyuiu.MolodchikovEE.Sprint5.Task1.V12.Lib
 {
@@ -6,22 +8,29 @@ namespace Tyuiu.MolodchikovEE.Sprint5.Task1.V12.Lib
     {
         public string SaveToFileTextData(int startValue, int stopValue)
         {
-            string output = "";
+            StringBuilder output = new StringBuilder();
+            string resultString = "";
             for (int i = startValue; i <= stopValue; i++) 
             {
-
                 double numerator = 5 * i + 2.5;
                 double denominator = Math.Sin(i) - 2;
 
                 if (denominator == 0)
                 {
-                    output += "0\n";
+                    output.Append("0");
                 }
                 else 
                 {
-                    output += $"{Math.Round((numerator / denominator) + 2, 2)}\n";
+                    output.Append($"{Math.Round((numerator / denominator) + 2, 2)}");
                 }
-            } return output;
+
+                if (i < stopValue) 
+                {
+                    output.Append("\n");
+                }
+                resultString = output.ToString();
+
+            } return resultString;
         }
     }
 }
