1073a73f7e6bfbde8febdb17ddef690166694f66
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-10-07 22:15:22 +0500

    Task3

--- Tyuiu.MolodchikovEE.Sprint5.Task3.V26.Lib/DataService.cs
+++ Tyuiu.MolodchikovEE.Sprint5.Task3.V26.Lib/DataService.cs
@@ -7,10 +7,10 @@ namespace Tyuiu.MolodchikovEE.Sprint5.Task3.V26.Lib
         public string SaveToFileTextData(int x)
         {
             double calculation = Math.Round((0.7 * (Math.Pow(x, 3))) + (1.52 * (Math.Pow(x, 2))), 3);
-            byte[] byteArray;
-            byteArray = BitConverter.GetBytes(calculation);
-            string base64String = Convert.ToBase64String(byteArray);
-            return base64String;
+            byte[] byteArray = BitConverter.GetBytes(calculation);
+            string filePath = "OutPutFileTask3.bin";
+            File.WriteAllBytes(filePath, byteArray);
+            return Path.GetFullPath(filePath);
         }
     }
 }
