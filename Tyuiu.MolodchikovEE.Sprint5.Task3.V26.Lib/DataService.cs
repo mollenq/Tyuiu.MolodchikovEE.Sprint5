5c063780652d2f86d5cd3497529fcb1e4faf622d
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-10-07 18:56:28 +0500

    Task3

--- Tyuiu.MolodchikovEE.Sprint5.Task3.V26.Lib/DataService.cs
+++ Tyuiu.MolodchikovEE.Sprint5.Task3.V26.Lib/DataService.cs
@@ -1,13 +1,15 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint5;
-
+﻿using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint5;
 namespace Tyuiu.MolodchikovEE.Sprint5.Task3.V26.Lib
 {
     public class DataService : ISprint5Task3V26
     {
+        
         public string SaveToFileTextData(int x)
         {
             double calculation = Math.Round((0.7 * (Math.Pow(x, 3))) + (1.52 * (Math.Pow(x, 2))), 3);
-            byte[] byteArray = BitConverter.GetBytes(calculation);
+            string result = calculation.ToString("F3");
+            byte[] byteArray = Encoding.UTF8.GetBytes(result);
             string base64String = Convert.ToBase64String(byteArray);
             return base64String;
         }
