5ae1d65a1c0e171444a6f22a4d1bc57ce406a12b
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-10-09 18:29:35 +0500

    Task5

--- Tyuiu.MolodchikovEE.Sprint5.Task5.V9.Lib/DataService.cs
+++ Tyuiu.MolodchikovEE.Sprint5.Task5.V9.Lib/DataService.cs
@@ -6,7 +6,22 @@ namespace Tyuiu.MolodchikovEE.Sprint5.Task5.V9.Lib
     {
         public double LoadFromDataFile(string path)
         {
-            throw new NotImplementedException();
+            string fileContent = File.ReadAllText(path);
+            var values = fileContent
+                .Split(new[] { ' ', '\n', '\r', ',', ';' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(val =>
+                {
+                    if (double.TryParse(val, out double doubleValue))
+                    {
+                        return Math.Round(doubleValue, 3);
+                    }
+                    return (double?)null;
+                })
+                .Where(val => val.HasValue)
+                .Select(val => val.Value)
+                .ToArray();
+            var maxInteger = values.Where(val => val == Math.Truncate(val)).Max();
+            return maxInteger;
         }
     }
 }
--- Tyuiu.MolodchikovEE.Sprint5.Task5.V9.Test/DataServiceTest.cs
+++ Tyuiu.MolodchikovEE.Sprint5.Task5.V9.Test/DataServiceTest.cs
@@ -4,7 +4,10 @@
     {
         public void TestMethod1()
         {
-            Assert.AreEqual(1, 1);
+            int x = 2;
+            double result = Math.Round((0.7 * (Math.Pow(x, 3))) + (1.52 * (Math.Pow(x, 2))), 3);
+            double wait = 11.68;
+            Assert.AreEqual(wait, result);
         }
     }
 }
