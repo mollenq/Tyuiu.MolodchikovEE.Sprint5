8e9153523fd3317035ec6c07a2dc4a58fc9ada52
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-10-09 12:32:21 +0500

    Task4

--- Tyuiu.MolodchikovEE.Sprint5.Task4.V17.Lib/DataService.cs
+++ Tyuiu.MolodchikovEE.Sprint5.Task4.V17.Lib/DataService.cs
@@ -7,15 +7,8 @@ namespace Tyuiu.MolodchikovEE.Sprint5.Task4.V17.Lib
     {
         public double LoadFromDataFile(string path)
         {
-            string fileContent = File.ReadAllText(path);
-            if (!double.TryParse(fileContent, out double x))
-            {
-                throw new FormatException("Содержимое файла не является допустимым вещественным числом.");
-            }
-            if (x == 0)
-            {
-                throw new InvalidOperationException("Значение из файла не может быть нулевым.");
-            }
+            string tempFilePath = Path.Combine(Path.GetTempPath(), "InPutDataFileTask4V17.txt");
+            double.TryParse(tempFilePath, out double x);
             double result = Math.Round(Math.Sin(2 / (3 * x)) + Math.Pow(x, 2), 3);
             return result;
         }
