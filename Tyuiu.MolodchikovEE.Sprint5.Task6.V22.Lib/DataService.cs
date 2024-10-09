eecf9d48c2a80b7ec86c6ebb0535633ceefe8824
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-10-09 19:32:44 +0500

    Task6

---
+++ Tyuiu.MolodchikovEE.Sprint5.Task6.V22.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MolodchikovEE.Sprint5.Task6.V22.Lib
+{
+    public class DataService : ISprint5Task6V22
+    {
+        public int LoadFromDataFile(string path)
+        {
+            string fileContent = File.ReadAllText(path);
+            int count = 0;
+            for (int i = 0; i < fileContent.Length - 1; i++)
+            {
+                if (fileContent[i] == 'м' && fileContent[i + 1] == 'м')
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.MolodchikovEE.Sprint5.Task6.V22/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.MolodchikovEE.Sprint5.Task6.V22.Lib;
+namespace Tyuiu.MolodchikovEE.Sprint5.Task6.V22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнил: Молодчиков Егор Евгеньевич | ИИПБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Обработка текстовых файлов                                              *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Выполнил: Молодчиков Егор Евгеньевич | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine(@"*Дан файл С:\DataSprint5\InPutDataFileTask6V22.txt (файл взять из архива с*");
+            Console.WriteLine(@"*согласно вашему варианту. Создать папку в ручную С:\DataSprint5\ и скопир*");
+            Console.WriteLine("*ровать в неё файл) в котором есть набор символьных данных. Найти количест*");
+            Console.WriteLine("*тво удвоенных букв мм в заданной строке.                               *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+
+            string path = @"C:\DataSprint5\InPutDataFileTask6V22.txt";
+            var result = ds.LoadFromDataFile(path);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
