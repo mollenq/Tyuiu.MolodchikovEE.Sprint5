b10298a0a4e3b05f807227427729fb7f9a847de2
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-10-09 17:53:28 +0500

    Task4

--- Tyuiu.MolodchikovEE.Sprint5.Task4.V17.Lib/DataService.cs
+++ Tyuiu.MolodchikovEE.Sprint5.Task4.V17.Lib/DataService.cs
@@ -12,9 +12,9 @@ namespace Tyuiu.MolodchikovEE.Sprint5.Task4.V17.Lib
             //Console.WriteLine(x);
             //double result = Math.Round(Math.Sin(2 / (3 * x)) + Math.Pow(x, 2), 3);
 
-            string tempFilePath = Path.GetTempFileName();
-            File.WriteAllText(tempFilePath, "5");
-            double x = Double.Parse(File.ReadAllText(tempFilePath));
+            //string tempFilePath = Path.GetTempFileName();
+            //File.WriteAllText(tempFilePath, "5");
+            double x = Double.Parse(File.ReadAllText(path));
             double result = Math.Round(Math.Sin(2 / (3 * x)) + Math.Pow(x, 2), 3);
             return result;
         }
